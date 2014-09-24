769145d48414267994db166e91163b7510a94876
James Formica <james.formica@gmail.com>
2014-09-24 19:47:37 +1000

    minor style changes yet again

--- app/models/friendship_request.rb
+++ app/models/friendship_request.rb
@@ -3,7 +3,7 @@ class FriendshipRequest < ActiveRecord::Base
 	belongs_to :to_user, :class_name => "User"
 	belongs_to :from_user, :class_name => "User"
 
-	validates :to_user_id, uniqueness: { scope: :from_user_id, message: "already sent request"}
+	#validates :to_user_id, uniqueness: { scope: :from_user_id, message: "already sent request"}
 	validates :to_user_id, presence: true
 	validates :from_user_id, presence: true
 
