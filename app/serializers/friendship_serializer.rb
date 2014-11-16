f20a9f14ab00dded3d22dbc41e73cf1bf521e813
James Formica <jamesformica@gmail.com>
2014-11-17 07:43:04 +1100

    make friends page somewhat worth looking at.. its getting better

--- app/serializers/friendship_serializer.rb
+++ app/serializers/friendship_serializer.rb
@@ -1,7 +1,15 @@
 class FriendshipSerializer < ActiveModel::Serializer
-	attributes :id, :friend_id, :friend_username
+	attributes :id, :friend_id, :friend_username, :highlight_colour
 
   def friend_username
   	object.friend.username
   end
+
+
+  def highlight_colour
+  	colours = ["#51c3f5", "#f59b1c", "#17efb1", "#ff8780", "#ff79c0", "#745eb0", "#efcc00", "#ef588c", "#65ff7e"]
+  	firstLetter = friend_username[0]
+  	return colours[firstLetter.ord % (colours.length - 1)]
+  end
+
 end
