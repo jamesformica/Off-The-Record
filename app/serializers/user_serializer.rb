7014d46780a1121b17084adc5df4c8f85ad1b6c7
James Formica <jamesformica@gmail.com>
2014-12-03 07:27:20 +1100

    add username to the side panel and make it link to the edit profile page

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :username
-  has_many :friendship_requests
-  has_many :friendships
-  has_many :questions
+	include UserHelper
+	
+	attributes :id, :name, :email, :username, :favourite_colour
+	has_many :friendship_requests
+	has_many :friendships
+	has_many :questions
+
+	def favourite_colour
+		AccentColour.get_color_from_name(object.username)
+	end
 end
