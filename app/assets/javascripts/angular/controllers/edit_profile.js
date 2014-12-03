f78a6d5ca8b6db4c5ddff53246473eafb5cd4927
James Formica <jamesformica@gmail.com>
2014-12-04 07:54:20 +1100

    if on edit profile and you refresh the page, it will redirect to the questions page

--- app/assets/javascripts/angular/controllers/edit_profile.js
+++ app/assets/javascripts/angular/controllers/edit_profile.js
@@ -3,6 +3,10 @@ angular.module("edit_profile.controller", [])
 .controller('EditProfileController', ['$scope', 'User', 'Loading',
 	function ($scope, User, Loading) {
 
+		if (!$scope.current_user) {
+			$scope.setCurrentViewable($scope.sections.questions.url);
+		}
+
 		$scope.edit_user = jQuery.extend(true, {}, $scope.current_user);
 
 		$scope.update_information = function(current_user) {
