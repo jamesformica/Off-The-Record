214f80da48f54d98fd6570478675f55313056f63
James Formica <jamesformica@gmail.com>
2014-12-09 07:38:22 +1100

    more css and directive clean up

--- app/assets/javascripts/angular/directives/common.js
+++ app/assets/javascripts/angular/directives/common.js
@@ -43,23 +43,6 @@ angular.module("common.directive", [])
 	}
 })
 
-.directive('otrHeader', function() {
-	return {
-		restrict: 'E',
-		replace: 'true',
-		template: '<div class="otr-home-banner" id="otr-nav"><div class="otr-home-logo"><h1 id="otr-heading-text" ng-model="heading_text"></h1></div></div>',
-		link: function(scope, elem, attrs) {
-			$(document).ready(function() {
-				var heading_text_div = $(elem).find("#otr-heading-text");
-				heading_text_div.text(attrs.title).arctext({radius: attrs.radius ? attrs.radius : 500 });
-			});
-		}
-	};
-
-
-
-})
-
 .directive('yToBottom', ['$timeout', function($timeout) {
 	return {
 		restrict: 'A',
@@ -143,16 +126,4 @@ angular.module("common.directive", [])
 	}
 }])
 
-
-.directive('backgroundColour', [function() {
-	return {
-		restrict: 'A',
-		scope: {
-			color: '@'
-		},
-		link: function (scope, element, attrs) {
-			$(element).css('background-color', scope.color);
-		}
-	}
-}])
 ;
